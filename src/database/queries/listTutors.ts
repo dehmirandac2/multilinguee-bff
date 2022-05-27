d37b3b3c8b3b3e23147d90f8289cacd96a986bb0
Deborah Miranda Casanova Costa <dehmirandac2@Deborahs-MacBook-Pro.local>
2022-05-27 11:39:25 -0300

    adjust query to get tutors

--- src/database/queries/listTutors.ts
+++ src/database/queries/listTutors.ts
@@ -5,13 +5,21 @@ const listTutors = async () => {
   const connection = await db();
 
   const [tutors] = await connection.execute(`
-    SELECT users.id, name, surname, MAX(about) AS about, ROUND(AVG(stars), 0) AS stars, COUNT(DISTINCT tutor_reviews.id) AS totalReviews, GROUP_CONCAT(DISTINCT language SEPARATOR ',') AS languages
-    FROM users 
-      INNER JOIN tutors ON users.id = tutors.userId
-      INNER JOIN tutors_languages ON users.id = tutors_languages.userId
-      INNER JOIN tutor_reviews ON users.id = tutor_reviews.userId
-      GROUP BY users.id;
-      `);
+  SELECT 
+    users.id,
+    name,
+    surname,
+    MAX(about) AS about,
+    CASE WHEN MAX(stars) IS NOT NULL
+      THEN ROUND(AVG(stars), 0)
+      ELSE 0 END AS stars,
+    COUNT(DISTINCT tutor_reviews.id) AS totalReviews, GROUP_CONCAT(DISTINCT language SEPARATOR ',') AS languages
+  FROM users 
+  INNER JOIN tutors ON users.id = tutors.userId
+  INNER JOIN tutors_languages ON users.id = tutors_languages.userId
+  LEFT JOIN tutor_reviews ON users.id = tutor_reviews.userId
+  GROUP BY users.id;
+  `);
 
       
 return tutors
